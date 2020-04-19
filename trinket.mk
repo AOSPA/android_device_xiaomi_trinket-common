26b4d0a3f8c3993779b3551c4e212e1d0704d635
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    trinket-common: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I4d1de21487757be3df3bd908e1a6152fd951d732

--- trinket.mk
+++ trinket.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.disable_backpressure=1
+    debug.sf.disable_backpressure=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Fingerprint
 PRODUCT_COPY_FILES += \
