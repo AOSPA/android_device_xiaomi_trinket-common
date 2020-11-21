757d1197084c02039172a76dfc19070be69998e9
Vishalcj17 <vishalcj@aospa.co>
2020-11-21 05:23:12 +0100

    laurel: use default trinket defconfig.
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ib872ddec567f055ca665c214461555a1a81da87c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,8 +84,6 @@ BOARD_KERNEL_CMDLINE := \
     swiotlb=1 \
     video=vfb:640x400,bpp=32,memsize=3072000
 
-TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
-
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
