37fdb017ae5cdaa3768aa381b40f59eb82d31126
Vishalcj17 <vishalcj@aospa.co>
2020-11-21 07:57:34 +0100

    [DNM] laurel: build permissive for now.
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I21873c6fe37297dce060841318eb2402cfafb9c4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,8 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237
     service_locator.enable=1 \
     swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
