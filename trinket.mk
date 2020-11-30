c8804d56e9bb4a7e1523fd209902b887d7ccbdc5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-30 19:56:32 +0530

    trinket-common: Enable support for IR transmitter

--- trinket.mk
+++ trinket.mk
@@ -119,6 +119,14 @@ PRODUCT_PACKAGES += \
     init.qti.dcvs.sh \
     init.target.rc
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.14
 
