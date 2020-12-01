d3f7bbca156bc14ca96cc5c593d6493d1d4360d9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-01 10:14:55 +0530

    trinket-common: Fix derp in kernel cmdline

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,7 +63,7 @@ BOARD_KERNEL_CMDLINE := \
     earlycon=msm_geni_serial,0x4a90000 \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237
+    msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=1 \
     video=vfb:640x400,bpp=32,memsize=3072000 \
