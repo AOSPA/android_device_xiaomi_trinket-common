1f16c18e9ecdf336f1d04dc60015427f0b2e6512
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-06 13:55:04 +0530

    trinket-common: Enable display color management
     * unlocks color modes in display settings after caf disabled
       it explicitly for trinket in commit
       https://source.codeaurora.org/quic/la/platform/hardware/qcom/display/commit/?h=LA.UM.9.11.r1-02400-NICOBAR.0&id=1b056c2223143ea401a47f8726a32b7e58656ab2

--- trinket.mk
+++ trinket.mk
@@ -118,7 +118,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early.app.duration=16500000 \
     debug.sf.earlyGl.sf.duration=13500000 \
     debug.sf.earlyGl.app.duration=21000000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.use_color_management=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1
