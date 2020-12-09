e6b5ccd83763c2656eb008e8acbcfbc5ba1fa779
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-09 17:44:57 +0530

    trinket-common: Switch SELinux to enforcing

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,8 +72,7 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
-    androidboot.selinux=permissive
+    video=vfb:640x400,bpp=32,memsize=3072000
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
